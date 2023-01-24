331de41dee5c6423a96541ab6e38da6866fb04a2
gurellia53 <gurellia53@gmail.com>
2023-01-23 20:01:59 -0600

    add battery2

--- plot.py
+++ plot.py
@@ -21,6 +21,8 @@ def plot():
             "Feta-12A-1-21-2023_Feta-12A-1-21-2023.csv",
             "Swiss-12A-1-21-2023_Swiss-12A-1-21-2023.csv",
             "Battery1-12A_1_23_2023_Battery1-12A_1_23_2023.csv",
+            "Camembert-12A-1-21-2023_Camembert-12A-1-21-2023.csv",
+            "Battery2-12A-1-21-2023_Battery2-12A-1-21-2023.csv",
             ]
     # get data from the list of files.
     data = [get_data(s) for s in fnames]
